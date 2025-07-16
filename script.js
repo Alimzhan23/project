2851e8f893cafbba8cd8c386a2bd2d92466c1641
Alimzhan23 <alimzhan5030666@gmail.com>
2025-07-16 18:06:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+let sections = document.querySelectorAll('section');
+
+let observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  section.classList.add('hidden');
+  observer.observe(section);
+});
